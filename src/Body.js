b15dcf3bce4929d46de677c84531bca0901c7dfb
abdi mussa <abdimussa87@gmail.com>
2020-10-16 12:34:05 +0300

    Added movie trailer watching functionlaity

--- src/Body.js
+++ src/Body.js
@@ -3,22 +3,54 @@ import './Body.css'
 import VideoCard from './VideoCard'
 import axios from './axios';
 import FlipMove from 'react-flip-move';
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer'
+
 function Body({ selectedOption }) {
     const [movies, setMovies] = useState([]);
+    const [trailerUrl, setTrailerUrl] = useState('');
     useEffect(() => {
         axios.get(selectedOption).then(response => {
             setMovies(response.data.results);
         });
 
-    }, [selectedOption])
+    }, [selectedOption]);
+
+    // * for youtube player
+    const opts = {
+        height: '390',
+        width: '640',
+        playerVars: {
+            // https://developers.google.com/youtube/player_parameters
+            autoplay: 1,
+        },
+    };
+    const handleClick = (movie) => {
+        // *if trailer url exists, set it to empty to close the youtube player
+        console.log("Clicking");
+        if (trailerUrl) {
+            setTrailerUrl('');
+        } else {
+            console.log(movie?.name);
+            movieTrailer(movie?.name || movie?.title || "").then(url => {
+                console.log(url);
+                const urlParams = new URLSearchParams(new URL(url).search);
+                setTrailerUrl(urlParams.get("v"));
+            }).catch(err => alert("Couldn't find the trailer"));
+        }
+    }
     return (
         <div className='body'>
             <FlipMove>
                 {
                     movies.map(movie =>
-                        <VideoCard key={movie.id} movie={movie} />
+                        <div onClick={() => handleClick(movie)} key={movie.id}>
+
+                            <VideoCard key={movie.id} movie={movie} />
+                        </div>
                     )}
             </FlipMove>
+            { trailerUrl && < YouTube videoId={trailerUrl || ""} opts={opts} />}
         </div>
     )
 }
